509dd1a18a2c83b99d67021b409ee35a06d29166
Tudor Alexandru <tudorale2005@gmail.com>
2021-08-01 12:21:29 +0300

    fixing UI bugs & initiating hire me section

--- components/About/About.js
+++ components/About/About.js
@@ -4,19 +4,27 @@ import World from "../../public/world.png";
 function About() {
     return (
         <div className={styles.about}>
+            
             <h1 className={styles.header}>About Me</h1>
-            <p className={styles.sectionOne}>First of all, my name is Tudor Alexandru - Ionut and I am from România, my passion for Web Development started in July 2019. I started with HTML5 and I didn&apos;t know so much about this, but I chose the Front-End field because I want to show the world what I can do with a computer. 
-            And now I am doing freelancing on UpWork and living a good life by connecting with people around the world.
-                <span className={styles.additionalWrapper}>
-                    <Image width="20" height="20" className={styles.imageWorld} alt="world" src={World}/>
-                </span>
-            </p>
+            <div className={styles.sectionOne}>
+                
+                First of all, my name is Tudor Alexandru - Ionut and I am from România, my passion for Web Development started in July 2019. I started with HTML5 and I didn&apos;t know so much about this, but I chose the Front-End field because I want to show the world what I can do with a computer. 
+                And now I am doing freelancing and living a good life by connecting with people around the world.  
+               
+                <div className={styles.imageWrapper}>
+                    <Image width="20" height="20" alt="world" src={World}/>
+                </div>    
+            </div>
+            
             <div className={styles.aboutBanner}>
                 <div className={styles.wrapper}>
-                    <p>After a few months I knew that I am good at what I&apos;m doing, actually, from the first page that I made at that time I knew that I can do much better. I wasn&apos;t like others at start, put some text and add a color and that&apos;s it, even though I didn&apos;t had the knowledge at that time to make something bigger, like animations or working with libraries, and I don&apos;t like to lie myself about my work, I want to make the things perfectly and I think I have a special eye for details.</p>
-                    <h1 className={styles.number}>02</h1>
+                    <div className={styles.additionalWrapper}>
+                        <p>After a few months I knew that I am good at what I&apos;m doing, actually, from the first page that I made at that time I knew that I can do much better. I wasn&apos;t like others at start, put some text and add a color and that&apos;s it, even though I didn&apos;t had the knowledge at that time to make something bigger, like animations or working with libraries, and I don&apos;t like to lie myself about my work, I want to make the things perfectly and I think I have a special eye for details.</p>
+                        <h1 className={styles.number}>02</h1>
+                    </div> 
                 </div>
             </div>
+
         </div>
     )
 }
---
+++ components/Hire/Hire.js
@@ -0,0 +1,18 @@
+import styles from "./Hire.module.css";
+
+function Hire() {
+    return (
+        <div className={styles.hire}>
+           <h1 className={styles.header}>Hire Me</h1>
+           <div className={styles.banner}>
+                <div className={styles.content}>
+                    <p>Do you have an idea that will make you a millionare but you need a website? Or you have a company that needs another man in the team? <b>I&apos;m here!</b></p>
+                
+                    <form></form>
+               </div>
+           </div>
+        </div>
+    )
+}
+
+export default Hire
