657b6b5a83e1e9ddc18d2c4e4963deb142f3b6cf
Tudor Alexandru <tudorale2005@gmail.com>
2021-08-19 20:27:09 +0300

    saving color on local storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 import "aos/dist/aos.css"
+import {useEffect} from "react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }){
+
+  useEffect(() => {  
+
+    if(localStorage.getItem('color') == null){
+      let root = document.querySelector(':root');
+      root.style.setProperty('--main-color', '#6930c3') // which is the default color for my website
+    }else{
+      // get the color from localstorage and apply it to the root
+      const savedColor = localStorage.getItem('color');
+      
+      let root = document.querySelector(':root');
+      root.style.setProperty('--main-color', `${savedColor}`)
+    }
+
+  }, [])
+  
   return <Component {...pageProps} />
 }
 
