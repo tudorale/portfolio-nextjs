f8639b81386dd6bf873be6a4e8ea32d998ba8522
Tudor Alexandru <tudorale2005@gmail.com>
2021-08-19 20:20:57 +0300

    removing unused code

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
 import '../styles/globals.css'
 import "aos/dist/aos.css"
-import {useEffect} from "react"
 
 function MyApp({ Component, pageProps }){
 
-  useEffect(() => {  
-
-      let root = document.querySelector(':root');
-      root.style.setProperty('--main-color', `#6930c3`)
-
-  }, [])
-  
   return <Component {...pageProps} />
 }
 
