83e8c1b36a082fdb39fe9926e839d089b8110a51
Tudor Alexandru <tudorale2005@gmail.com>
2021-08-23 20:19:21 +0300

    removing AOS, adding smooth scrollbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import '../styles/globals.css'
-import "aos/dist/aos.css"
+// import "aos/dist/aos.css"
+import Scrollbar from "smooth-scrollbar";
+import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }){
 
-  return <Component {...pageProps} />
+  useEffect(() => {
+    let elem = document.querySelector("body");
+
+    Scrollbar.init(elem);
+
+  }, [])
+
+  return (
+      <Component {...pageProps} />
+  )
 }
 
 export default MyApp
