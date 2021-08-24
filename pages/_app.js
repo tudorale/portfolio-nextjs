c17c88178d717e345e7e09fb6747832df5fa4df1
Tudor Alexandru <tudorale2005@gmail.com>
2021-08-24 18:50:42 +0300

    styling scrollbar & removing unused dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
 import '../styles/globals.css'
 import "aos/dist/aos.css"
-import {useEffect} from "react";
-import Scrollbar from "smooth-scrollbar";
 
 function MyApp({ Component, pageProps }){
-
-  useEffect(() => {
-    const myScrollbar = Scrollbar.init(document.querySelector("#container"));
-
-    [].forEach.call(document.querySelectorAll('[data-aos]'), (el) => {
-      myScrollbar.addListener(() => {
-        if (myScrollbar.isVisible(el)) {
-          el.classList.add('aos-animate');
-        }
-      });
-    });
-  }, [])
-
   return (
       <div id="container">
         <Component {...pageProps} />
