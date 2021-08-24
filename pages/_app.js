9b88679fa85612487cfb04244dd86f140dfdf631
Tudor Alexandru <tudorale2005@gmail.com>
2021-08-24 18:37:01 +0300

    adding smooth scrollbar with AOS

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import '../styles/globals.css'
 import "aos/dist/aos.css"
 import {useEffect} from "react";
+import Scrollbar from "smooth-scrollbar";
 
 function MyApp({ Component, pageProps }){
 
+  useEffect(() => {
+    const myScrollbar = Scrollbar.init(document.querySelector("#container"));
+
+    [].forEach.call(document.querySelectorAll('[data-aos]'), (el) => {
+      myScrollbar.addListener(() => {
+        if (myScrollbar.isVisible(el)) {
+          el.classList.add('aos-animate');
+        }
+      });
+    });
+  }, [])
+
   return (
-      <Component {...pageProps} />
+      <div id="container">
+        <Component {...pageProps} />
+      </div>
   )
 }
 
