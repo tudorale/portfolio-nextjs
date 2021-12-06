333ea642c8097a24835e567020155191efcccf03
Tudor Alexandru <tudorale2005@gmail.com>
2021-12-06 09:07:11 +0200

    adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,52 @@
 import '../styles/globals.css'
 import "aos/dist/aos.css"
+import Head from "next/head"
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as ga from '../lib/ga'
+import {configs} from "../config"
 
 function MyApp({ Component, pageProps }){
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
-      <div id="container">
-        <Component {...pageProps} />
-      </div>
+      <>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${configs.ga}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${configs.ga}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <div id="container">
+          <Component {...pageProps} />
+        </div>
+      </>
   )
 }
 
