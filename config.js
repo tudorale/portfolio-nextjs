81bfc0d815421daf2a6c743202e4f10d52c5acf1
Tudor Alexandru <tudorale2005@gmail.com>
2022-02-21 19:51:20 +0200

    dynamic case study page

--- config.js
+++ config.js
@@ -4,7 +4,3 @@ export let configs = {
     userId: "user_eBPDRrNR92trQmc95skaP",
     ga: "G-04BQ3SRXCE",
 }
-
-const dev = process.env.NODE_ENV !== 'production';
-
-export const server = dev ? 'http://localhost:3000' : 'https://tudoralexandru.vercel.app';
