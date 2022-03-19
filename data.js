abe79dee05356d216f5d2313121b9f938ebc7540
Tudor Alexandru <tudorale2005@gmail.com>
2022-03-19 15:51:04 +0200

    case-studies

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 export const paths = {
-    paths: ["aibloc"]
+    paths: ["aibloc", "onlyfrens", "mememarketplace", "skullbots"]
 }
 
 export const projects = [
@@ -31,6 +31,138 @@ export const projects = [
         },
         links: {
                 github: "https://github.com/tudorale/aibloc",
+                website: false,
+        },
+        summery: [
+                "Ai Bloc is a non-code platform where you can build Data Science and Machine Learning programs, the client wanted a good and beautiful landing page for this start-up.",
+                "At first, I started designing a wireframe in Figma, the client was happy about it and then we went to the coding process. I used mainly React JS as the client was fine with it, I used also different libraries such as GSAP, AOS, Typed.js for animations. The illustrations from the landing page were edited in Adobe Photoshop to match the color scheme."
+        ],
+        role: [
+              "As the recruiter for this one time project wanted a Front-End Developer, with experience in React JS, I handled the coding of the landing page but also the design of it.",
+              "For the coding process I used React JS with the following libraries: GSAP, AOS, React Slick and Typed JS. To design the wireframe and a mockup for the landing page I used Figma."
+        ],
+        walkthrough: [
+                "The project started with a wireframe made in Figma, after the client review it, I started adding more features, illustrations and icons to make the design look better. After the mock up for the landing page was done I started the coding process.",
+                "As I said I made this project in React JS as the client had his platform also on React JS, this will help the client to implement the landing page to his main project, the coding process took between 10-24 hours of coding, the first day I added the content for the website, the second day we found illustrations and made carousel for the videos that were provided by the client, and in the final day I made the website fully responsive.",
+                "The landing page turned out as a beautiful responsive website with animations and a great user experience as you can see in the images below."
+        ]
+    },
+    {
+        path: "onlyfrens",
+        title: "OnlyFrens",
+        description: "OnlyFrens is a NFT collection, my role was to turn a Figma Design into this beautiful landing page for this beautiful idea.",
+        images: {
+                imageOne: {
+                    src: "/casestudies/onlyfrens/o1.png", 
+                    desc: "Hero section"
+                },
+                imageTwo: {
+                    src: "/casestudies/onlyfrens/o2.png",
+                    desc: "Section two"
+                },
+                imageThree: {
+                    src: "/casestudies/onlyfrens/o3.png",
+                },
+                imageFour: {
+                    src: "/casestudies/onlyfrens/o4.png",
+                },
+                imageFive: {
+                    src: "/casestudies/onlyfrens/o5.png",
+                },
+                imageSix: {
+                    src: "/casestudies/onlyfrens/o6.png",
+                },
+        },
+        links: {
+                github: "https://github.com/tudorale/onlyfrens",
+                website: false,
+        },
+        summery: [
+                "Ai Bloc is a non-code platform where you can build Data Science and Machine Learning programs, the client wanted a good and beautiful landing page for this start-up.",
+                "At first, I started designing a wireframe in Figma, the client was happy about it and then we went to the coding process. I used mainly React JS as the client was fine with it, I used also different libraries such as GSAP, AOS, Typed.js for animations. The illustrations from the landing page were edited in Adobe Photoshop to match the color scheme."
+        ],
+        role: [
+              "As the recruiter for this one time project wanted a Front-End Developer, with experience in React JS, I handled the coding of the landing page but also the design of it.",
+              "For the coding process I used React JS with the following libraries: GSAP, AOS, React Slick and Typed JS. To design the wireframe and a mockup for the landing page I used Figma."
+        ],
+        walkthrough: [
+                "The project started with a wireframe made in Figma, after the client review it, I started adding more features, illustrations and icons to make the design look better. After the mock up for the landing page was done I started the coding process.",
+                "As I said I made this project in React JS as the client had his platform also on React JS, this will help the client to implement the landing page to his main project, the coding process took between 10-24 hours of coding, the first day I added the content for the website, the second day we found illustrations and made carousel for the videos that were provided by the client, and in the final day I made the website fully responsive.",
+                "The landing page turned out as a beautiful responsive website with animations and a great user experience as you can see in the images below."
+        ]
+    },
+    {
+        path: "mememarketplace",
+        title: "Meme Marketplace",
+        description: "OnlyFrens is a NFT collection, my role was to turn a Figma Design into this beautiful landing page for this beautiful idea.",
+        images: {
+                imageOne: {
+                    src: "/casestudies/meme/m1.png", 
+                    desc: "Hero section"
+                },
+                imageTwo: {
+                    src: "/casestudies/meme/m2.png",
+                    desc: "Section two"
+                },
+                imageThree: {
+                    src: "/casestudies/meme/m3.png",
+                },
+                imageFour: {
+                    src: "/casestudies/meme/m4.png",
+                },
+                imageFive: {
+                    src: "/casestudies/meme/m5.png",
+                },
+                imageSix: {
+                    src: "/casestudies/meme/m5.png",
+                },
+        },
+        links: {
+                github: "https://github.com/tudorale/meme",
+                website: false,
+        },
+        summery: [
+                "Ai Bloc is a non-code platform where you can build Data Science and Machine Learning programs, the client wanted a good and beautiful landing page for this start-up.",
+                "At first, I started designing a wireframe in Figma, the client was happy about it and then we went to the coding process. I used mainly React JS as the client was fine with it, I used also different libraries such as GSAP, AOS, Typed.js for animations. The illustrations from the landing page were edited in Adobe Photoshop to match the color scheme."
+        ],
+        role: [
+              "As the recruiter for this one time project wanted a Front-End Developer, with experience in React JS, I handled the coding of the landing page but also the design of it.",
+              "For the coding process I used React JS with the following libraries: GSAP, AOS, React Slick and Typed JS. To design the wireframe and a mockup for the landing page I used Figma."
+        ],
+        walkthrough: [
+                "The project started with a wireframe made in Figma, after the client review it, I started adding more features, illustrations and icons to make the design look better. After the mock up for the landing page was done I started the coding process.",
+                "As I said I made this project in React JS as the client had his platform also on React JS, this will help the client to implement the landing page to his main project, the coding process took between 10-24 hours of coding, the first day I added the content for the website, the second day we found illustrations and made carousel for the videos that were provided by the client, and in the final day I made the website fully responsive.",
+                "The landing page turned out as a beautiful responsive website with animations and a great user experience as you can see in the images below."
+        ]
+    },
+    {
+        path: "skullbots",
+        title: "Skullbots Biker Gang",
+        description: "OnlyFrens is a NFT collection, my role was to turn a Figma Design into this beautiful landing page for this beautiful idea.",
+        images: {
+                imageOne: {
+                    src: "/casestudies/skullbots/s1.png", 
+                    desc: "Hero section"
+                },
+                imageTwo: {
+                    src: "/casestudies/skullbots/s2.png",
+                    desc: "Section two"
+                },
+                imageThree: {
+                    src: "/casestudies/skullbots/s3.png",
+                },
+                imageFour: {
+                    src: "/casestudies/skullbots/s4.png",
+                },
+                imageFive: {
+                    src: "/casestudies/skullbots/s5.png",
+                },
+                imageSix: {
+                    src: "/casestudies/skullbots/s6.png",
+                },
+        },
+        links: {
+                github: "https://github.com/tudorale/hellswap",
                 website: false,
         },
         summery: [
